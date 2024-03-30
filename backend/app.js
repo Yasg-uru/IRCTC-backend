867d0265c04f49943b59affda6d4105b3a3a4b73
yasg-uru <yashpawar12122004@gmail.com>
2024-03-30 13:07:10 +0530

    authentication system is created

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
 import BookingRouter from "./router/Booking.router.js"
 import TrainRouter from "./router/Train.router.js"
+import userRouter from "./router/user.route.js"
 const app=express()
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cookieParser())
+app.use("/api/user",userRouter)
 app.use("/api/Train",TrainRouter)
 app.use("/api/Book",BookingRouter)
 
