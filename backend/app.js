f8387199477b52e8d43d4dc24538e4b9800bc4bb
yasg-uru <yashpawar12122004@gmail.com>
2024-04-09 23:02:43 +0530

    testing by ping pong

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,9 @@ app.use(cors({
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cookieParser())
+app.get('/ping', (req, res) => {
+    res.send('PONG')
+});
 app.use("/api/user",userRouter)
 app.use("/api/Train",TrainRouter)
 app.use("/api/Book",BookingRouter)
