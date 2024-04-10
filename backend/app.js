196d39eb18b8eca04d655139c62826de0fa25c25
yasg-uru <yashpawar12122004@gmail.com>
2024-04-11 00:42:11 +0530

    added the new origin

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ import userRouter from "./router/user.route.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://irctc-backend-mmjp.vercel.app",
     credentials:true
 
 }))
