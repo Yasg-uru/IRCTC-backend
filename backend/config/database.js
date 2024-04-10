e4c2c7a3c1c80d8fcb1c99ed6d34de4bbe686654
yasg-uru <yashpawar12122004@gmail.com>
2024-04-11 00:06:03 +0530

    second

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 const connectDatabase=async function(){
 try {
-    const response=await mongoose.connect(process.env.MONGO_URL,{
-        useNewUrlParser: true, useUnifiedTopology: true
-    })
+    const response=await mongoose.connect(process.env.MONGO_URL)
     console.log(`database is connected with ${response.connection.host }`)
 } catch (error) {
     console.log(`error is occured in connection of database :${error?.message}`)
