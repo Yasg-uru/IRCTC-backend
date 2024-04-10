991e52fa0aa5e1d9d7113e7877dfb8fc2e2cb7c0
yasg-uru <yashpawar12122004@gmail.com>
2024-04-10 10:13:59 +0530

    corrected env config andn gitgnore

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js"
 import dotenv from "dotenv"
 import connectDatabase from "./config/database.js"
-dotenv.config({path:"backend/config/config.env"})
+dotenv.config({path:"config/config.env"})
 connectDatabase()
 const PORT=process.env.PORT || 4001
 app.listen(PORT,()=>{
