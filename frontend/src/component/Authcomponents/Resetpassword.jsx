3e4f508edb241bb4cc809c6b8e53200bb8c081e3
yasg-uru <yashpawar12122004@gmail.com>
2024-04-12 00:18:18 +0530

    reset password

--- frontend/src/component/Authcomponents/Resetpassword.jsx
+++ frontend/src/component/Authcomponents/Resetpassword.jsx
@@ -15,7 +15,7 @@ function Resetpassword() {
   function handlesubmitform(event) {
     event.preventDefault();
     console.log("this is formdata of the resetpassword:",formdata)
-    if (!FormData.password || !FormData.confirmpassword) {
+    if (FormData.password!== FormData.confirmpassword) {
       toast.error("please enter the valid password");
       return ;
     }
