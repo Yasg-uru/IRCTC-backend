1e1e0e0828d4d3b00ee9c358bd1c3fc7c3c39c08
yasg-uru <yashpawar12122004@gmail.com>
2024-04-11 19:55:06 +0530

    addes middleware in searctrain

--- backend/router/Train.router.js
+++ backend/router/Train.router.js
@@ -7,10 +7,11 @@ import {
   gettrains,
   searchtrainbyorigintodestination,
 } from "../controller/Train.controller.js";
+import { isAuthenticated } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.route("/create").post(create_Train);
-router.route("/search").post(searchtrainbyorigintodestination);
+router.route("/search").post(isAuthenticated,searchtrainbyorigintodestination);
 router.route("/getavailability").post(getAvailableSeatCountsForAllCoachTypes);
 router.route("/price/ticket").post(getcostofticket);
 router.route("/assignseat").post(assignseatsforallcoaches);
