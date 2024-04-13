2836e2642eea282f65303164173b8375bd1c957c
yasg-uru <yashpawar12122004@gmail.com>
2024-04-13 21:26:23 +0530

    traih

--- backend/router/Train.router.js
+++ backend/router/Train.router.js
@@ -11,7 +11,7 @@ import { isAuthenticated } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.route("/create").post(create_Train);
-router.route("/search").post(isAuthenticated,searchtrainbyorigintodestination);
+router.route("/search").post(searchtrainbyorigintodestination);
 router.route("/getavailability").post(getAvailableSeatCountsForAllCoachTypes);
 router.route("/price/ticket").post(getcostofticket);
 router.route("/assignseat").post(assignseatsforallcoaches);
